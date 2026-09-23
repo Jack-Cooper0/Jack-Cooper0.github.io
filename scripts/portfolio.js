bbe705cc206acf79fcad36bca955f7e08918cfb3
Alejandro <alejandro.batista@insbaixcamp.cat>
2026-09-23 20:27:00 +0200

    retoques en los logos

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,6 +1,7 @@
 
 const disc = 'texto'
 
+
 function copiarTexto(texto) {
 
     navigator.clipboard.writeText(texto).then(() => {
@@ -12,4 +13,12 @@ function copiarTexto(texto) {
     }).catch(err => {
         console.error('Error al copiar el texto: ', err);
     });
+    const aviso = document.createElement('div');
+  aviso.className = 'aviso-copiado';
+  aviso.textContent = '⚠️ Discord copiado al portapapeles';
+
+  document.body.appendChild(aviso);
+
+  // Lo borra a los 2 segundos
+  setTimeout(() => aviso.remove(), 2000);
 }
